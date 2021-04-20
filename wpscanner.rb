d73a9d120512ef76256dfc2f12821f70ab640faa
krishpranav <krisna.pranav@gmail.com>
2021-04-20 15:12:54 +0530

    added banners

--- wpscanner.rb
+++ wpscanner.rb
@@ -16,5 +16,21 @@ system("clear")
 puts "WPSCANNER".red
 
 while true do
-    print "track>> : ".red
+    print ">> : ".red
     $option = gets.chomp
+    if $option == "exit" then
+        break 
+    end
+    
+    if $option == "help" then 
+
+        puts
+
+        puts "[site] MAKE YOUR TARGET".green
+         puts "[help] show this MESSAGE".green
+        puts "[targ] Search targets".green
+        puts "[exit] exit the script".green
+        puts "[uptd] Update wpscanner".green
+        puts "[anon] Run Anonymous Mode".green
+        puts "[info] About wpscanner".green
+        puts
